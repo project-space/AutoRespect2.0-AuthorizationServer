209dd90020b739de325326f61faa350f0ab482cc
alex troshkin <alextroshkin@outlook.com>
2017-09-27 21:50:40 +0300

    Remove db project [moved to autorespect-database]

--- src/AutoRespect.AuthorizationServer.DataBase/Migrations/1_CreateTableUser.sql
+++
@@ -1,8 +0,0 @@
-﻿-- <Migration ID="1a6c30bb-3daf-4eeb-a498-4a0ce25ec6e4" />
-CREATE TABLE Users (
-	Id INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
-	Login varchar (64) NOT NULL,
-	Password VARCHAR (32) NOT NULL
-)
-
-GO
