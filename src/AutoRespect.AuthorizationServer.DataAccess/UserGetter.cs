5f21f498fd72480a2d7337e158244759dc45947c
alex troshkin <alextroshkin@outlook.com>
2017-10-29 21:52:56 +0300

    Added DI to data-access layer

--- src/AutoRespect.AuthorizationServer.DataAccess/UserGetter.cs
+++ src/AutoRespect.AuthorizationServer.DataAccess/UserGetter.cs
@@ -1,28 +1,35 @@
-﻿using System.Data.SqlClient;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using AutoRespect.AuthorizationServer.Design.Interfaces.DataAccess;
 using AutoRespect.AuthorizationServer.Design.Models;
 using AutoRespect.AuthorizationServer.Design.Primitives;
+using AutoRespect.Infrastructure.DataAccess.Design;
+using AutoRespect.Infrastructure.DI.Design;
+using AutoRespect.Infrastructure.DI.Design.Attributes;
 using AutoRespect.Infrastructure.ErrorHandling;
-using Dapper;
 
 namespace AutoRespect.AuthorizationServer.DataAccess
 {
+    [DI(LifeCycleType.Singleton)]
     public class UserGetter : IUserGetter
     {
-        private readonly string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AutoRespect.IdentityServer;Integrated Security=SSPI;";
+        private readonly IIdentityServerDb db;
+
+        public UserGetter(IIdentityServerDb db)
+        {
+            this.db = db;
+        }
 
         public async Task<Result<User>> Get(Login login)
         {
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                const string sqlScript = @"select * from  Account where Login = @Login";
+            var query = new Query(
+                @"select * from  Account where Login = @Login",
+                new { Login = login.Value }
+            );
 
-                var user = await connection.QueryFirstOrDefaultAsync<User>(sqlScript, new { Login = login.Value });
-                if (user == null) return new UserNotFound(login);
+            var user = await db.QuerySingleOrDefault<User>(query);
+            if (user == null) return new UserNotFound(login.Value);
 
-                return user;
-            }
+            return user;
         }
     }
 }
--- src/AutoRespect.AuthorizationServer.DataAccess/UserSaver.cs
+++ src/AutoRespect.AuthorizationServer.DataAccess/UserSaver.cs
@@ -2,14 +2,23 @@ using System.Data.SqlClient;
 using System.Threading.Tasks;
 using AutoRespect.AuthorizationServer.Design.Interfaces.DataAccess;
 using AutoRespect.AuthorizationServer.Design.Models;
+using AutoRespect.Infrastructure.DataAccess.Design;
+using AutoRespect.Infrastructure.DI.Design;
+using AutoRespect.Infrastructure.DI.Design.Attributes;
 using AutoRespect.Infrastructure.ErrorHandling;
 using Dapper;
 
 namespace AutoRespect.AuthorizationServer.DataAccess
 {
+    [DI(LifeCycleType.Singleton)]
     public class UserSaver : IUserSaver
     {
-        private readonly string _connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AutoRespect.IdentityServer;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private readonly IIdentityServerDb db;
+
+        public UserSaver(IIdentityServerDb db)
+        {
+            this.db = db;
+        }
 
         public async Task<Result<int>> Save(User user)
         {
@@ -35,14 +44,13 @@ namespace AutoRespect.AuthorizationServer.DataAccess
 
                 select @Id";
 
-            using (var connection = new SqlConnection(_connectionString))
-            {
-                return await connection.QueryFirstAsync<int>(sqlScript, new { 
-                    Id       = user.Id,
-                    Login    = user.Login.Value,
-                    Password = user.Password.Value 
-                });
-            }
+            var query = new Query(sqlScript, new {
+                Id       = user.Id,
+                Login    = user.Login.Value,
+                Password = user.Password.Value
+            });
+
+            return await db.Execute(query);
         }
     }
 }
