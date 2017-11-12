db06b9b826910d0575df6dc56d7b23ea77a760a3
alex troshkin <alextroshkin@outlook.com>
2017-11-12 13:37:50 +0300

    [startup] change host url

--- src/AutoRespect.AuthorizationServer.Api/Program.cs
+++ src/AutoRespect.AuthorizationServer.Api/Program.cs
@@ -6,9 +6,10 @@ namespace AutoRespect.AuthorizationServer.Api
     public class Program
     {
         public static void Main(string[] args) => BuildWebHost(args).Run();
-    
+
         public static IWebHost BuildWebHost(string[] args) => WebHost
             .CreateDefaultBuilder(args)
+            .UseUrls("http://localhost:10001")
             .UseStartup<Startup>()
             .Build();
     }
